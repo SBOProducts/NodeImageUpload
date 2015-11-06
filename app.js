5b8480d3a90b8218c7a919f90f4d2b220c4c37eb
nathantownsendFKQ <ntownsend@fkq.com>
2015-11-06 15:32:49 -0500

    added static file supportfor the index.html page, and changed the path of the handlers to /test

--- app.js
+++ app.js
@@ -3,10 +3,30 @@ var app = express();
 
 app.listen(3000);
 
-app.get('/', function(req, res){
-  res.send('hello world');
+// this middleware will be executed for every request to the app
+app.use(function (req, res, next) {
+  console.log('Time: %d', Date.now());
+  next();
+})
+
+// return static files from the public folder
+app.use(express.static(__dirname + '/public'));
+
+// get request to the /test path
+app.get('/test', function(req, res){
+	console.log('get request called');
+	console.log('req.app holds a reference to the express app instantiated at the app.js level');
+  	res.send('hello world');
 });
 
+// post request to the /test path
+app.post('/test', function (req, res) {
+	console.log('post request called');
+  	res.send('POST request to homepage');
+});
+
+// hook up express app to the expport
 module.exports = app;
 
+// show that we're started
 console.log("The app is up and running!");
